d166d70b4606f194f72a2edf0ca76cae64dfd635
Babel-India <84664679+Babel-India@users.noreply.github.com>
2022-01-19 20:34:40 +0530

    first commit

---
+++ src/Components/BSTC.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import "./Styles/BSTC.css";
+const BSTC = () => {
+  return (
+    <div id="BSTC" className='bstc'> 
+            <h1 className='bstc-head'>Babel Software Testing Certification Champions</h1>
+            <h3 className='bstc-intro'>There are always new, grander challenges to confront, and a true winner will embrace each one.</h3>
+            <Container>
+               <h1 className='bstc-name'>August</h1>
+
+               <table className="bstc-table">
+                   <tr>
+                       <th>Name</th>
+                       <th>Marks</th>
+                       <th>Name</th>
+                       <th>Marks</th>
+                   </tr>
+                   <tr>
+                       <th>Ashish Kanyal</th>
+                       <th>33</th>
+                       <th>Abhishek Singh</th>
+                       <th>35</th>
+                   </tr>
+                   <tr>
+                       <th>Ankit kumar</th>
+                       <th>38</th>
+                       <th>Jhalak Puri</th>
+                       <th>35</th>
+                   </tr>
+                   <tr>
+                       <th>Saksham Singh</th>
+                       <th>36</th>
+                       <th>Kunal Senapati</th>
+                       <th>35</th>
+                   </tr>
+                   <tr>
+                       <th>Biplab Halder</th>
+                       <th>37</th>
+                       <th>Vishal Sairwal</th>
+                       <th>37</th>
+                   </tr>
+                   <tr>
+                       <th>Namit Mehra</th>
+                       <th>37</th>
+                       <th>Akansha Singh</th>
+                       <th>37</th>
+                   </tr>
+</table>
+            </Container>
+        </div>
+  );
+};
+
+export default BSTC;
---
+++ src/Components/Poem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Styles/BSTC.css';
+
+function Poem(){
+    return(
+        <div className='poem-head'>
+            <h1 className='game-head'>Most Common Broken New Years Resolution:</h1>
+            <div className='poem'>
+            <p className='poem-line'>
+            <h4>Lose weight and get fit</h4>
+                It's one of the most common New Year's resolutions. After a season of way too many cookies, candies and holiday parties, it's only natural that a vow to lose weight and get fit would follow. Each January, fitness clubs offer deals and promotions to those who want to make good on their resolutions. To those who have been at the gym for the other 11 months of the year, the crowded classes and treadmill lines make the new year a dreaded time. Luckily for gym rats, research says that 60% of gym memberships go unused and attendance is usually back to normal by mid-February. Why is that? Simple: people are lazy.
+            <br/><br/>
+            <h4>Quit Smoking</h4>
+            So you want to quit smoking? You should. It yellows your teeth, infiltrates all your clothing, irritates your significant other and charms only those in an ever dwindling crowd of fellow smokers. So what better time than now?
+Good luck. Only an estimated 15% of people who try to quit manage to stay cigarette-free six months later. True, there are a host of products to help wean you off your nicotine addiction — patches, chewing gum, "e-cigarettes" and even "snus," a Scandinavian habit of wedging a tobacco capsule up against one's gum. The last two, though, have fallen afoul of the FDA; some say the agency overly frets about the harmful effects of these products, which, after all, can stop people from indulging in far more carcinogenic cigarettes. In any event, smokers, you have your work cut out for you. Think it over during your next cigarette break.
+            <br/><br/>
+            <h4>Learn Something</h4>
+            You've been meaning to learn French. You'd love to play the piano. How great would it be to really know how to cook? You'll read Mastering the Art of French Cooking, and, man, you'll master it. You can even work on your French at the same time! Resolving to learn something new is exciting: the world is full of fascinating facts, skills and talents. And the process of discovering them, not just the end result, is enjoyable and rewarding.
+At least, for a while. Soon you remember there's a reason you haven't learned all this yet. French is too hard to pronounce. Piano takes too much practice. Ordering out is just so much easier than cooking. You'll do it ... when you have more time.
+            <br/><br/>
+            <h4>Get Out Of Debt and Save Money</h4>
+            After a particularly trying financial year (and the always budget-unfriendly month of December), consumers might call for a halt to spending and vow to manage their debt more effectively. With unemployment still high, the sad fact about this 2011 resolution is that many people will have difficulty accomplishing it, and not through any fault of their own. Financial planners advise making specific budget-friendly rules, rather than pronouncing overarching and often unattainable goals. Only allow yourself to eat one dinner out a week. Take a sack lunch to work most days. Vow to shop around for a new auto, home insurance and credit cards with lower interest rates and free balance transfers. Can you spare an extra $50 or even $20 a month to put toward paying off your student loans?
+If you're lucky enough to make it out of debt, the goal then becomes tucking some money away for retirement or a rainy day. Look into maxing out your 401(k) at work. And, while flipping through your favorite magazine, repeat the mantra, "I will save money this year, I will save money this year." But, ooh, look at that new Marc Jacobs bag, and I really could use a Kindle for all those subway commutes. Oh, and what about a new wardrobe for spring? Hmm. Maybe I'll save money next year.
+            <br/><br/>
+            <h4>Spend More Time With Family</h4>
+            Everyone's busy these days, it's true. But blood is thicker than water, and the beginning of the year is an ideal time to reconnect with family that you haven't seen in a while. Great idea, right? Then February arrives, reality sets in, and you realize that the reason you didn't see cousin Jim more often is because he really isn't that interesting at all. Or that plan to spend more time with the kids? Well, it turns out that work doesn't magically disappear with the dawning of a new year, and you're at the office more than ever. It's a hard promise to keep — no matter how sincere the desire.
+            <br/>
+                        <p>~Yash Bhatnagar</p>
+
+            </p>
+
+        </div>                   
+        </div>
+    )
+}
+
+export default Poem;
